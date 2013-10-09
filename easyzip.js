5ad448dd5484c99c84d126bcb1c92ceddba506f5
Nahuel Scotti <nahuel.scotti@gmail.com>
2013-10-09 17:55:54 +0200

    add "options" parameter in addFolder method to change the name of rootFolder

--- easyzip.js
+++ easyzip.js
@@ -24,11 +24,11 @@ EasyZip.prototype.addFile = function(file,filePath,callback){
 	var datas = [],
 		me = this,
 		rs = fs.createReadStream(filePath);
-	
+
 	rs.on('data',function(data){
 		datas.push(data);
 	})
-	
+
 	rs.on('end',function(){
 		var buf = Buffer.concat(datas);
 		me.file(file, toArrayBuffer(buf),{base64:false, binary: true});
@@ -45,11 +45,11 @@ EasyZip.prototype.batchAdd = function(files,callback) {
 			folder = item.folder,
 			fileName = path.basename(target),
 			dirname = path.dirname(target);
-		
+
 		if(dirname!='.'){
 			appender = me.folder(dirname);
 		}
-		
+
 		if(source != null && source.trim()!=''){
 			appender.addFile(fileName,source,function(){
 				callback();
@@ -59,29 +59,30 @@ EasyZip.prototype.batchAdd = function(files,callback) {
 			me.folder(target);
 			callback();
 		}
-		
+
 	},function(){
 		callback(me);
 	});
 }
 
 
-EasyZip.prototype.zipFolder = function(folder,callback) {
+EasyZip.prototype.zipFolder = function(folder, callback, options) {
 	if(!fs.existsSync(folder)){
 		callback(new Error('Folder not found'),me);
 	}else{
+		options = options || {};
 		var me = this,
 			files = fs.readdirSync(folder),
-			rootFolder = path.basename(folder),
+			rootFolder = options.rootFolder || path.basename(folder),
 			zips = [],
 			file,stat,targetPath,sourcePath;
-		
+
 		while(files.length > 0){
 			file = files.shift();
 			sourcePath = path.join(folder,file);
 			targetPath = path.join(rootFolder,file);
 			stat = fs.statSync(sourcePath);
-			
+
 			if(stat.isFile()){
 				zips.push({
 					target : targetPath,
@@ -91,19 +92,19 @@ EasyZip.prototype.zipFolder = function(folder,callback) {
 				zips.push({
 					target : targetPath
 				});
-				
+
 				//join the path
 				async.map(fs.readdirSync(sourcePath),function(item,callback){
 					callback(null,path.join(file,item));
 				},function(erro,result){
 					files = files.concat(result);
 				});
-				
+
 			}
 		}
-		
+
 		me.batchAdd(zips,function(){callback(null,me)});
-		
+
 	}
 }
 
